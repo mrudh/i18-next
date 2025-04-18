499cfc681b496ba32c8433a6465e0829c8a144f4
mrudh <mrudhulaa0405@gmail.com>
2025-04-18 07:32:28 +0530

    changes

--- src/i18.js
+++ src/i18.js
@@ -13,7 +13,7 @@ i18n.use(LanguageDetector).use(initReactI18next).init({
         },
         fr: {
             translation : {
-                greeting : "Bonjour",
+                greeting : "Bonjour!"
             }
         },
         hi: {
