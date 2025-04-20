a9e059aa7653f71c7c9ad96f3b13aeffc0159400
mrudh <mrudhulaa0405@gmail.com>
2025-04-20 22:49:09 +0530

    backend

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './i18.js'
+import './i18n.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Suspense fallback="loading.....">
+      <App />
+    </Suspense>
+
   </StrictMode>,
 )
